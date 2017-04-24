2ef0c217d370c3b9d892a868c6d0a52bf8e49bc1
Faisal Mohamed <faisalmohamed@Faisals-MacBook-Pro.local>
2017-04-24 09:57:57 -0400

    finished twitter and spotify functions

--- keys.js
+++ keys.js
@@ -4,5 +4,5 @@ exports.twitterKeys = {
   consumer_key: 'dv29XErXvOt6wVYhQBCGeP6Y2',
   consumer_secret: 'xE7XD5tYBNsdz85Lq2vEY3POX9KOZoDdwk1rMVD1wg2gizBPbm',
   access_token_key: '856153727878795264-GPD1SahDTAp70CT4CjY6hAJccMftSJX',
-  access_token_secret: 'BDApA2KNlYsoVPC1VPc8tGgnviirVziKObx1qGgmUndiZ',
+  access_token_secret: 'BDApA2KNlYsoVPC1VPc8tGgnviirVziKObx1qGgmUndiZ'
 }
---
+++ liri.js
@@ -0,0 +1,136 @@
+// main variables
+var nodeArgs = process.argv;
+var operator = nodeArgs[2];
+var searchItem = '';
+
+	// if multi word searchItem
+	for (var i=3; i<nodeArgs.length; i++) {
+		if (i>3 && i<nodeArgs.length) {
+			searchItem = searchItem + '+' + nodeArgs[i];
+		} else {
+			searchItem = searchItem + nodeArgs[i];
+		}
+	}
+
+var keys = require('./keys.js');
+var keysList = keys.twitterKeys;
+var request = require('request');
+var fs = require('fs');
+
+// switch statement to determine action
+switch (operator) {
+	case 'my-tweets':
+		twitter();
+		break;
+	case 'spotify-this-song':
+		spotify();
+		break;
+	case 'movie-this':
+		movie();
+		break;
+	case 'do-what-it-says':
+		random();
+		break;
+}
+
+// functions for 'my-tweets', 'spotify-this-song', 'movie-this', and 'do-what-it-says'
+
+// 'my-tweets': 20 most recent tweets and date/time
+function twitter() {
+	var Twitter = require('twitter');
+	var client = new Twitter({
+		consumer_key: keysList.consumer_key,
+		consumer_secret: keysList.consumer_secret,
+		access_token_key: keysList.access_token_key,
+		access_token_secret: keysList.access_token_secret
+	});
+	var params = {
+		screen_name: 'ttweettest',
+		count: 20
+	};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			console.log('----------------------------------------');
+			console.log('User: @' + params.screen_name);
+			console.log('20 most recent tweets');
+			console.log('----------------------------------------');
+			for (var i=0; i<tweets.length; i++) {
+				console.log('Tweet: ' + tweets[i].text);
+				console.log('Date/Time: ' + tweets[i].created_at);
+				console.log('----------------------------------------');
+			}
+		}
+	});
+}
+
+// spotify-this-song: return song information
+function spotify() {
+	var spotify = require('spotify');
+
+	switch (searchItem) {
+		case '':
+			spotify.search({type: 'track', query: 'come+together'}, function(err, data) {
+				if (err) {
+					console.log('Error occurred: ' + err);
+					return;
+				}
+				var songObject = data.tracks.items[0];
+				songInfo(songObject);
+			})
+			break;
+		default:
+			spotify.search({ type: 'track', query: searchItem }, function(err, data) {
+				if (err) {
+					console.log('Error occurred: ' + err);
+					return;
+				}
+				var songObject = data.tracks.items[0];
+				songInfo(songObject);
+			})
+	}
+}
+
+// display song information
+function songInfo(songObject) {
+	console.log('----------------------------------------');
+	console.log('Artist(s): ' + songObject.artists[0].name);
+	console.log('Song: ' + songObject.name);
+	console.log('Album: ' + songObject.album.name);
+	console.log('Preview: ' + songObject.preview_url);
+	console.log('----------------------------------------');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
